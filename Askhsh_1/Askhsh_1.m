af2db57442b3fb1395a31fbf1b3758126e841e94
LuckyPunk <moutafis@tem.uoc.gr>
2013-11-29 20:45:44 +0200

    Telikh morfh askhsewn

--- Askhsh_1/Askhsh_1.m
+++ Askhsh_1/Askhsh_1.m
@@ -1,34 +1,25 @@
-function T = Askhsh_1 (A, b)
+function [T,time] = Askhsh_1 (A, b)
 %
-%
+% Epilyei to systhma y = Ax - b me thn me8odo Jordan pivot.
+% Xrhsimopoioume tis me8odous tobl kai ljx tou bibliou, Linear
+%Programming with MATLAB.
+% Epistrefei akomh kai ton xrono ekteleshs.
+%Syntax: [T,time] = Askhsh_1(A,b).
 %
 
 T = totbl(A, b);
 
 [m,n] = size(A);
-previous_Is = zeros(m);
 previous_Js = zeros(n);
+tic;
     for i = 1:m-1
         for j = 1:n
             if T.val(i,j) ~= 0 && previous_Js(j) ~= 1
-                previous_Is(i) = 1;
                 previous_Js(j) = 1;
                 T = ljx(T, i, j);
                 break
             end
         end
     end
-    
-    count_zeros = 0;
-    for i = 1:m
-        if previous_Is(i) == 0
-            for j = 1:n
-                if previous_Js(j) == 0
-                    if T.val(i,j) == 0 
-                        count_zeros = count_zeros + 1; 
-                    end
-                end
-            end
-        end
-    end
+time = toc;
 end
--- Askhsh_1/Askhsh_2.m
+++ Askhsh_1/Askhsh_2.m
@@ -1,41 +1,49 @@
-function T = Askhsh_2(A,b,p)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [T,time] = Askhsh_2(A,b,p)
+%
+% Epilyei provlhma elaxistopoihshs grammikou programmatismou, me thn
+%me8odo Simplex.
+% Xrhsimopoioume tis me8odous tobl kai ljx tou bibliou, Linear
+%Programming with MATLAB.
+% Epistrefei akomh kai ton xrono ekteleshs.
+%Syntax: [T,time] = Askhsh_2(A,b,p)
+%
+
 T = totbl(A,b,p);
 
 flag = 0;
 [m,n] = size(T.val);
 
+tic;
 while flag == 0
     [Elem_1,Col] = min(T.val(m,1:n-1));
     
     if Elem_1 >= 0
         flag = 1;
     else
-        tmp_vec = T.val(1:m-1,n);
-        for i = 1:m
-            [Elem_2, Row] = min(tmp_vec);
-            if Elem_2 < 0
-            tmp_vec(Row) = 1000;
+        check_vec = (-T.val(1:m-1,n))./(T.val(1:m-1,Col));
+        check_flag = 0;
+        while check_flag == 0
+            [Elem_2,Row] = min(check_vec);
+            if Elem_2 <= 0
+                check_vec(Row) = 1000;
             else
-                break
-            end     
+                T = ljx(T,Row,Col);
+                check_flag = 1;
+            end
         end
-        T = ljx(T,Row,Col);
     end
 end
-fprintf('\n\n\nH aristh lush einai : \n\n\n');
+time = toc;
+fprintf('\n\n\nH aristh lush (X*) einai: \n');
 for i = 1:m-1 
-    if strcmp(T.bas(i),'x1')==1
-        fprintf('x1= %f \n', T.val(i,n)); 
-    elseif strcmp(T.bas(i),'x2')==1
-       fprintf('x2= %f \n', T.val(i,n)); 
-    elseif strcmp(T.bas(i),'x3')==1
-       fprintf('x3= %f \n', T.val(i,n)); 
+    str_cat = strcat('x',num2str(i));
+    ind = find(ismember(T.bas,str_cat));
+    if size(ind) ~= 0
+        fprintf('%s = %f \n',str_cat,T.val(ind,n));   
     else
-        fprintf('x%d =0 \n', i);  
+        fprintf('%s = 0 \n', str_cat);  
     end
 end
-fprintf('\n\n To min(ÐÃÐ)= %f\n\n\n',T.val(m,n));
+fprintf('\nTo min(ÐÃÐ)= %f\n\n\n',T.val(m,n));
 end
 
